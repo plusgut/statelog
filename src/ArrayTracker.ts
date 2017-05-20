ef2703139acde86fd911b089066700740a3e91c3
plusgut <github@webentwickler2-0.de>
2017-05-20 14:45:57 +0200

    [tslint] activated tslint and made private functions explictly private

--- src/ArrayTracker.ts
+++ src/ArrayTracker.ts
@@ -1,81 +1,75 @@
 type Callback = (type: string, changedIds: number[], changedIndexes: number[]) => void;
 
 class ArrayTracker {
-  _idIncrement: number;
-  _target: any;
-  _shell: number[];
-  _changes: any;
-  _callback: Callback;
+  private idIncrement: number;
+  private target: any;
+  private shell: number[];
+  private callback: Callback;
 
   constructor(target: any[], callback: Callback) {
-    this._target = target;
-    this._callback = callback;
-    this._shell = [];
-    this.clearChanges();
+    this.target = target;
+    this.callback = callback;
+    this.shell = [];
 
-    for (this._idIncrement = 0; this._idIncrement < target.length; this._idIncrement += 1) {
-      this._shell.push(this._idIncrement);
+    for (this.idIncrement = 0; this.idIncrement < target.length; this.idIncrement += 1) {
+      this.shell.push(this.idIncrement);
     }
   }
 
-  clearChanges() {
-    this._changes = {};
-  }
-
-  get(target: any, property: string, proxy: any) {
+  public get(target: any, property: string, proxy: any) {
     switch (property) {
       case 'push': 
-        return this._push.bind(this);
+        return this.push.bind(this);
       case 'unshift': 
-        return this._unshift.bind(this);
+        return this.unshift.bind(this);
       case 'splice': 
-        return this._splice.bind(this);
+        return this.splice.bind(this);
       default:
-        if (this._target.hasOwnProperty(property)) {
+        if (this.target.hasOwnProperty(property)) {
           // @TODO add proxy for nesting
           // @TODO add caching
-          return this._target[property];
+          return this.target[property];
         } else {
-          return this._target[property];
+          return this.target[property];
         }
     }
   }
 
-  set() {
+  public set() {
     throw new Error('Setting values is not yet possible');
   }
 
-  _push(...entities: any[]) {
+  private push(...entities: any[]) {
     const changedIds     = [];
     const changedIndexes = [];
     for (let i = 0; i < entities.length; i += 1) {
-      this._idIncrement += 1;
-      this._shell.push(this._idIncrement);
-      changedIds.push(this._idIncrement);
-      changedIndexes.push(this._target.length + 1);
+      this.idIncrement += 1;
+      this.shell.push(this.idIncrement);
+      changedIds.push(this.idIncrement);
+      changedIndexes.push(this.target.length + 1);
     }
-    const result = this._target.push.apply(this._target, entities);
-    this._callback('create', changedIds, changedIndexes);
+    const result = this.target.push.apply(this.target, entities);
+    this.callback('create', changedIds, changedIndexes);
 
     return result;
   }
 
-  _unshift(...entities: any[]) {
+  private unshift(...entities: any[]) {
     const changedIds = [];
     const changedIndexes = [];
     for (let i = 0; i < entities.length; i += 1) {
-      this._idIncrement += 1;
-      this._shell.unshift(this._idIncrement);
-      changedIds.push(this._idIncrement);
+      this.idIncrement += 1;
+      this.shell.unshift(this.idIncrement);
+      changedIds.push(this.idIncrement);
       changedIndexes.push(i);
     }
-    const result = this._target.unshift.apply(this._target, entities);
-    this._callback('create', changedIds, changedIndexes);
+    const result = this.target.unshift.apply(this.target, entities);
+    this.callback('create', changedIds, changedIndexes);
 
     return result;
   }
 
-  _splice(start: number, deleteCount: number, ...entities: any[]) {
+  private splice(start: number, deleteCount: number, ...entities: any[]) {
     const createIndexes = [];
     const createIds     = [];
     const deleteIndexes = [];
@@ -87,25 +81,21 @@ class ArrayTracker {
     }
 
     for (let createIndex = 0; createIndex < entities.length; createIndex += 1) {
-      this._idIncrement += 1;
-      shellArgs.push(this._idIncrement);
-      createIds.push(this._idIncrement);
+      this.idIncrement += 1;
+      shellArgs.push(this.idIncrement);
+      createIds.push(this.idIncrement);
       createIndexes.push(start + createIndex);
     }
 
-    const deleteIds = this._shell.splice.apply(this._shell, shellArgs);
+    const deleteIds = this.shell.splice.apply(this.shell, shellArgs);
 
-    const result = this._target.splice.apply(this._target, arguments);
-    this._callback('delete', deleteIds, deleteIndexes);
-    this._callback('create', createIds, createIndexes);
+    const result = this.target.splice.apply(this.target, arguments);
+    this.callback('delete', deleteIds, deleteIndexes);
+    this.callback('create', createIds, createIndexes);
     return result;
   }
 
-  _pop() {
-
-  }
-
-  _addChanges(type: string) {
+  private pop() {
 
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,28 +7,28 @@ type Callback = (
 import ArrayTracker from './ArrayTracker';
 
 class StateLog {
-  proxyHandler: ArrayTracker;
-  proxy: any;
-  _callbacks: {callback: Callback, type: string}[];
+  public proxyHandler: ArrayTracker;
+  public proxy: any;
+  private callbacks: {callback: Callback, type: string}[];
 
   constructor(target: any) {
-    this._callbacks = [];
+    this.callbacks = [];
     if (Array.isArray) {
-      this.proxyHandler =  new ArrayTracker(target, this._trigger.bind(this));
+      this.proxyHandler =  new ArrayTracker(target, this.trigger.bind(this));
       this.proxy = new (<any>window).Proxy(target, this.proxyHandler);
     } else {
       throw new Error('Only arrays are implemented yet');
     }
   }
 
-  on(type: string, callback: Callback) {
-    this._callbacks.push({ type, callback });
+  public on(type: string, callback: Callback) {
+    this.callbacks.push({ type, callback });
   }
 
-  _trigger(type: string, changedIds:number[], changedIndexes:number[]) {
-    for (let i = 0; i < this._callbacks.length; i += 1) {
-      if (type === this._callbacks[i].type) {
-        this._callbacks[i].callback(changedIds, changedIndexes, this);
+  private trigger(type: string, changedIds:number[], changedIndexes:number[]) {
+    for (let i = 0; i < this.callbacks.length; i += 1) {
+      if (type === this.callbacks[i].type) {
+        this.callbacks[i].callback(changedIds, changedIndexes, this);
       }
     }
   }
